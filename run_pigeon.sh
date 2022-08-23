54dc72dccaf0c68f7454c4166a5897596d0afae0
sohyuyuan <sohyuyuan@healthmetrics.co>
2022-08-23 18:53:13 +0800

    Update run_pigeon.sh script
    - Rename generator files names

--- run_pigeon.sh
+++ run_pigeon.sh
@@ -1,8 +1,8 @@
 flutter pub run pigeon \
   --input pigeons/zendesk_flutter.dart \
-  --dart_out lib/pigeon.dart \
-  --objc_header_out ios/Classes/pigeon.h \
-  --objc_source_out ios/Classes/pigeon.m \
-  --experimental_swift_out ios/Classes/Pigeon.swift \
-  --java_out ./android/src/main/java/com/hms/zendesk_flutter/Pigeon.java \
+  --dart_out lib/zendesk_pigeon.dart \
+  --objc_header_out ios/Classes/ZendeskPigeon.h \
+  --objc_source_out ios/Classes/ZendeskPigeon.m \
+  --experimental_swift_out ios/Classes/ZendeskPigeon.swift \
+  --java_out ./android/src/main/java/com/hms/zendesk_flutter/ZendeskPigeon.java \
   --java_package "com.hms.zendesk_flutter"
